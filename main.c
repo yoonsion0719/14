5ea1821747fc1a515c684cabf8e3b3885a344b72
yoonsion0719 <yoonsonia0719@gmail.com>
2023-12-07 15:13:05 +0900

    14-1

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main(void) {
+	
+	char *pc = NULL;
+	int i=0;
+	
+	pc = (char*)malloc(100*sizeof(char));
+	if (pc==NULL){
+		printf("malloc failed\n");
+		exit(1);
+	}
+	
+	for (i=0;i<26;i++){
+		pc[i]='a'+i;
+	}
+	pc[i]=0;
+	printf("%s\n",pc);
+	free(pc);
+	
+}
