e1d653547661407bb19137d00f008491014a5494
yoonsion0719 <yoonsonia0719@gmail.com>
2023-12-07 15:45:19 +0900

    14-3

--- main.c
+++ main.c
@@ -9,22 +9,32 @@ struct Book {
 	char title[10];
 };
 
-void main(void) {
-	struct Book *p;
+
+void main(void){
+	int i;
+	struct Book **bookshelf;
 	
-	p = (struct Book *)malloc(2*sizeof(struct Book));
+	bookshelf=(struct Book**)malloc(3*sizeof(struct Book*));
+	//이놈은 2차원 포인터 & 구조체 3개 할당 
+	for (i=0;i<3;i++)
+		bookshelf[i]= (struct Book*)malloc(10*sizeof(struct Book));
+		//이친구는 1차원 포인터 & 구조체 10개 할당 
+		
+	bookshelf[1][3].number=5;
+	strcpy(bookshelf[1][3].title, "C++ Programming");
 	
-	if (p==NULL) {
-		printf("malloc failed\n");
-		return;
+	(bookshelf[2]+4)->number=3;
+	strcpy((bookshelf[2]+4)->title, "Communications Theory");
+	
+	printf("book (1,3): %i, %s\n",(bookshelf[1]+3)->number, (bookshelf[1]+3)->title);
+	printf("book (2,4): %i, %s\n",bookshelf[2][4].number, bookshelf[2][4].title);
+	
+	for (i=0;i<3;i++){
+		free(bookshelf[i]);
 	}
-	p->number=1;
-	strcpy(p->title, "C Programming");
+	//각 행에 할당된 메모리 해제 & 구조체 포인터 3개가 각각 가르키는 주소 해제 
+	free(bookshelf);
+	//전체 배열에 대한 메모리 해제 & 구조체 이중 포인터가 가르키는 주소 해제 
 	
-	(p+1)->number=2;
-	strcpy((p+1)->title,"Electronics");
-	
-	free(p);
 	return;
-	
 }
